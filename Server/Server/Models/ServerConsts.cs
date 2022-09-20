866918e2742d4c3164d5534e8febfbb2a8d23b67
Sergey-Vashenko <56011021+Sergey-Vashenko@users.noreply.github.com>
2022-09-20 12:47:14 +0700

    Add documentation.

---
+++ Server/Server/Models/ServerConsts.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Models
+{
+    /// <summary>
+    /// Класс, в котором хранятся все константы сервера.
+    /// </summary>
+    public class ServerConsts
+    {
+        /// <summary>
+        /// Высота отправляемого прямоугольника.
+        /// </summary>
+        public const int HEIGHT_OF_RECTANGLE = 25;
+
+        /// <summary>
+        /// Ширина отправляемого прямоугольника.
+        /// </summary>
+        public const int WIDTH_OF_RECTANGLE = 75;
+    }
+}
--- Server/Server/Services/GreeterService.cs
+++ Server/Server/Services/GreeterService.cs
@@ -1,22 +1,45 @@
 using Grpc.Core;
 using ConnectionComponents.Enums;
 using Microsoft.AspNetCore.SignalR.Protocol;
+using Server.Models;
 
 namespace GrpcService.Services
 {
+    /// <summary>
+    /// gRPC сервис для отправки необходимых координат.
+    /// </summary>
     public class GreeterService : Greeter.GreeterBase
     {
         #region Private Fields
 
+        /// <summary>
+        /// Максимальное количество прямоугольников.
+        /// </summary>
         private const int MAX_COUNT_RECTANGLES = 1000;
+
+        /// <summary>
+        /// Список со всеми пользователями-клиентами.
+        /// </summary>
         private static List<int> _usersList = new();
+
+        /// <summary>
+        /// Логгер для записи исключений.
+        /// </summary>
         private readonly ILogger<GreeterService> _logger;
+
+        /// <summary>
+        /// Список всех прямоугольников.
+        /// </summary>
         private List<RectangleReply> _rectanglesArray = new(MAX_COUNT_RECTANGLES);
 
         #endregion Private Fields
 
         #region Public Constructors
 
+        /// <summary>
+        /// Инициализирует экземпляр класса <see cref="GreeterService"/>.
+        /// </summary>
+        /// <param name="logger">Логгер для сервиса.</param>
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
@@ -27,11 +50,11 @@ namespace GrpcService.Services
         #region Public Methods
 
         /// <summary>
-        ///
+        /// Производит подключение к серверу.
         /// </summary>
-        /// <param name="request"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
+        /// <param name="request">Запрос на подключение.</param>
+        /// <param name="context"><inheritdoc/></param>
+        /// <returns>Экземпляр класса <see cref="ConnectionReply"/>, который является ответом сервера.</returns>
         public override Task<ConnectionReply> Connect(ConnectionRequest request, ServerCallContext context)
         {
             ConnectionReply reply = new();
@@ -47,6 +70,12 @@ namespace GrpcService.Services
             return Task.FromResult(reply);
         }
 
+        /// <summary>
+        /// Отправляет экземпляр класса <see cref="RectangleReply"/> клиенту.
+        /// </summary>
+        /// <param name="empty">Пустой запрос.</param>
+        /// <param name="responseStream">Поток данных, в котором хранятся отпраляемые <see cref="RectangleReply"/>.</param>
+        /// <param name="context"><inheritdoc/></param>
         public override async Task SendRectangle(
             EmptyMessage empty,
             IServerStreamWriter<RectangleReply> responseStream,
@@ -54,11 +83,11 @@ namespace GrpcService.Services
         {
             while (context.CancellationToken.IsCancellationRequested == false)
             {
-                RectangleReply rectangle = new RectangleReply
+                RectangleReply rectangle = new()
                 {
                     Id = _rectanglesArray.Count(),
-                    Height = 25,
-                    Width = 75,
+                    Height = ServerConsts.HEIGHT_OF_RECTANGLE,
+                    Width = ServerConsts.WIDTH_OF_RECTANGLE,
                     X = Random.Shared.Next(0, MAX_COUNT_RECTANGLES),
                     Y = Random.Shared.Next(0, MAX_COUNT_RECTANGLES),
                 };
@@ -66,7 +95,8 @@ namespace GrpcService.Services
                 _rectanglesArray.Add(rectangle);
 
                 await responseStream.WriteAsync(rectangle);
-                await Task.Delay(TimeSpan.FromSeconds(10), context.CancellationToken);
+
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
             }
         }
 
